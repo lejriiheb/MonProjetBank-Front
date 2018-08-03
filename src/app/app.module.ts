6891712509c6eb53721dbed3bcd1d7249fb9e430
Lejri Iheb <iheb.lejri@esprit.tn>
2018-08-03 16:15:41 +0200

    Commit routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,29 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ContentComponent } from './content/content.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { DetailCompteComponent } from './detail-compte/detail-compte.component';
+
+const tableRoutage = [
+  {path: 'comptes', component: ContentComponent},
+  {path: '', component: WelcomeComponent},
+  {path: 'comptes/:id', component: DetailCompteComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ContentComponent,
-    FooterComponent
+    FooterComponent,
+    WelcomeComponent,
+    DetailCompteComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(tableRoutage)
   ],
   providers: [],
   bootstrap: [AppComponent]
